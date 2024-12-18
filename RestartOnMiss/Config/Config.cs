01701fc5ff9f2d5437192b99bdca5c902f77d70d
SmoothButter <49420958+Smooth-Butter@users.noreply.github.com>
2024-12-17 19:56:22 -0500

    BLEH

--- RestartOnMiss/Config/Config.cs
+++ RestartOnMiss/Config/Config.cs
@@ -2,6 +2,14 @@
 {
     public class Config
     {
+/// Planned features/settings
+///
+/// - enable/disable obv
+/// - max number of miss
+/// - number of miss <= to PB
+/// - number of miss < PB
+/// - 
+        
         
     }
 }
