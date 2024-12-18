cb21b07d5f627f48b9d41a46cb22a304f522e59c
SmoothButter <49420958+Smooth-Butter@users.noreply.github.com>
2024-12-18 16:54:30 -0500

    stupid stupid stupid installer that there has to be a way of avoiding

---
+++ RestartOnMiss/Installers/MenuInstaller.cs
@@ -0,0 +1,13 @@
+﻿using RestartOnMiss.Views;
+using Zenject;
+
+namespace RestartOnMiss.Installers
+{
+    internal class MenuInstaller : Installer
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<ModUI>().FromNewComponentAsViewController().AsSingle().NonLazy();
+        }
+    }
+}
