3d77fe8d8b95aeede98a52874b5a1b976d8ea572
SmoothButter <49420958+Smooth-Butter@users.noreply.github.com>
2024-12-17 19:57:24 -0500

    was using harmony patches, am gonna use bsutils bc i dont wanna
    probs leave it here commented out incase need to go back

--- RestartOnMiss/Harmony/HarmonyPatches.cs
+++ RestartOnMiss/Harmony/HarmonyPatches.cs
@@ -1,7 +1,51 @@
-﻿namespace RestartOnMiss.Harmony
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace RestartOnMiss.Harmony
 {
-    public class HarmonyPatches
+    // Patch for AudioTimeSyncController.StopSong
+    [HarmonyPatch(typeof(AudioTimeSyncController))]
+    [HarmonyPatch("StopSong")]
+    public class StopSongPatch
     {
-        
+        public static void Postfix(AudioTimeSyncController __instance)
+        {
+            // Access the RestartOnMissController instance
+            RestartOnMissController controller = RestartOnMissController.Instance;
+
+            if (controller == null)
+            {
+                Plugin.Log.Warn("RestartOnMissController instance not found.");
+                return;
+            }
+        }
     }
+
+    // Patch for NoteController.HandleNoteDidPassMissedMarkerEvent
+    //[HarmonyPatch(typeof(NoteController))]
+    //[HarmonyPatch("HandleNoteDidPassMissedMarkerEvent")]
+    //public class NoteMissedPatch
+    //{
+    //    // Postfix runs after the original method
+    //    public static void Postfix(NoteController __instance)
+    //    {
+    //      // Access the RestartOnMissController instance
+    //        RestartOnMissController controller = RestartOnMissController.Instance;
+
+    //        if (controller == null)
+    //        {
+    //            Plugin.Log.Warn("RestartOnMissController instance not found.");
+    //            return;
+    //        }
+
+    //       // Ensure the note is not dissolving
+    //        if (__instance.dissolving)
+    //            return;
+
+    //        // Trigger the restart logic
+    //       controller.OnNoteMissed(__instance);
+    //    }
+    //}
+    
+    
 }
