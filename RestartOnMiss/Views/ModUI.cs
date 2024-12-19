195140dbd4f0fccb1854d2d09b2b6495a659a492
SmoothButter <49420958+Smooth-Butter@users.noreply.github.com>
2024-12-19 02:19:17 -0500

    added new settings!!

--- RestartOnMiss/Views/ModUI.cs
+++ RestartOnMiss/Views/ModUI.cs
@@ -25,6 +25,34 @@ namespace RestartOnMiss.Views
                 }
             }
         }
+        
+        [UIValue("countbombs")]
+        public bool CountBombs
+        {
+            get => PluginConfig.Instance.CountBombs;
+            set
+            {
+                if (PluginConfig.Instance.CountBombs != value)
+                {
+                    PluginConfig.Instance.CountBombs = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        
+        [UIValue("countbadcuts")]
+        public bool CountBadCuts
+        {
+            get => PluginConfig.Instance.CountBadCuts;
+            set
+            {
+                if (PluginConfig.Instance.CountBadCuts != value)
+                {
+                    PluginConfig.Instance.CountBadCuts = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
 
         [UIValue("maxMiss")]
         public int maxMisses
