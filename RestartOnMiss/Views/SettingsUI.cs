de817c06f2ef8f6bb64bcee64beb515283327ecc
SmoothButter <49420958+Smooth-Butter@users.noreply.github.com>
2024-12-19 13:26:47 -0500

    setup future settings

--- RestartOnMiss/Views/SettingsUI.cs
+++ RestartOnMiss/Views/SettingsUI.cs
@@ -23,6 +23,34 @@ namespace RestartOnMiss.Views
                 }
             }
         }
+        
+        [UIValue("enabledinreplay")]
+        public bool EnabledInReplay
+        {
+            get => PluginConfig.Instance.EnableInReplay;
+            set
+            {
+                if (PluginConfig.Instance.EnableInReplay != value)
+                {
+                    PluginConfig.Instance.EnableInReplay = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        
+        [UIValue("enabledinfpfc")]
+        public bool EnabledInFPFC
+        {
+            get => PluginConfig.Instance.EnableInFPFC;
+            set
+            {
+                if (PluginConfig.Instance.EnableInFPFC != value)
+                {
+                    PluginConfig.Instance.EnableInFPFC = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
 
         [UIValue("maxMiss")]
         public int maxMisses
