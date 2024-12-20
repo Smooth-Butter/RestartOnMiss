55c367b693017c5484525da26eb312c2b8286925
SmoothButter <49420958+Smooth-Butter@users.noreply.github.com>
2024-12-19 21:32:50 -0500

    added the stuff for SS

--- RestartOnMiss/ReplayFpfc/ReplayDetection.cs
+++ RestartOnMiss/ReplayFpfc/ReplayDetection.cs
@@ -1,57 +1,60 @@
 ﻿using HarmonyLib;
-using IPA.Utilities;
 using System.Reflection;
-using UnityEngine;
-using BeatLeader;
 using BeatLeader.Models;
+using RestartOnMiss.Harmony.ScoreSaberPatch;
 
-
-// thank you Meivyn for the help/example
+// thank you Meivyn for the help/example :)
 
 namespace RestartOnMiss.ReplayFpfc.ReplayDetection
 {
     public class ReplayDetector
     {
         private static bool BeatLeaderReplay;
+        private static bool ScoreSaberReplay;
         
         
-        private static MethodBase TargetMethod() => AccessTools.Method("Scoresaber.Core.Daemons.ReplayService:NewPlayStarted");
+        private static MethodBase TargetMethod() => AccessTools.Method("ScoreSaber.Core.Daemons.ReplayService:NewPlayStarted");
         
-        //private static bool ScoreSaberReplay()
-        //{
-            //return ScoreSaber.Plugin.ReplayState != null && ScoreSaber.Plugin.ReplayState.IsPlaybackEnabled
-        //}
-
-        public static void AddBLEvents()
+        public static void AddReplayEvents()
         {
             BeatLeader.Replayer.ReplayerLauncher.ReplayWasStartedEvent += SetBLTrue;
             BeatLeader.Replayer.ReplayerLauncher.ReplayWasFinishedEvent += SetBLFalse;
+            SSReplayPatches.ReplayWasStartedEvent += SetSSTrue;
+            SSReplayPatches.ReplayWasFinishedEvent += SetSSFalse;
         }        
         
-        public static void RemoveBLEvents()
+        public static void RemoveReplayEvents()
         {
             BeatLeader.Replayer.ReplayerLauncher.ReplayWasStartedEvent -= SetBLTrue;
             BeatLeader.Replayer.ReplayerLauncher.ReplayWasFinishedEvent -= SetBLFalse;
+            SSReplayPatches.ReplayWasStartedEvent -= SetSSTrue;
+            SSReplayPatches.ReplayWasFinishedEvent -= SetSSFalse;
         }
 
         private static void SetBLTrue(ReplayLaunchData replayLaunchData)
         {
             BeatLeaderReplay = true;
-            Plugin.Log.Info("Replay set true");
         }
         
         private static void SetBLFalse(ReplayLaunchData replayLaunchData)
         {
             BeatLeaderReplay = false;
-            Plugin.Log.Info("Replay set false");
         }
 
+        public static void SetSSTrue()
+        {
+            ScoreSaberReplay = true;
+        }
+        
+        public static void SetSSFalse()
+        {
+            ScoreSaberReplay = false;
+        }
 
         
         public static bool IsInReplay()
         {
-            return BeatLeaderReplay ;//|| ScoreSaberReplay()
+            return BeatLeaderReplay || ScoreSaberReplay;
         }
-
     }
 }
