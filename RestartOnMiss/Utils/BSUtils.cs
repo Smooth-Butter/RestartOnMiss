137ea316920e3f466fa4245afb0ba27512465244
SmoothButter <49420958+Smooth-Butter@users.noreply.github.com>
2024-12-23 04:46:21 -0500

    so basically im the goat and this now handles multi and missions like i want it to

--- RestartOnMiss/Utils/BSUtils.cs
+++ RestartOnMiss/Utils/BSUtils.cs
@@ -1,12 +1,16 @@
 ﻿using System.Linq;
+using BS_Utils.Gameplay;
 using UnityEngine;
 using BS_Utils.Utilities;
-
+using RestartOnMiss.Configuration;
+using RestartOnMiss.ReplayFpfc.FpfcDetection;
+using RestartOnMiss.ReplayFpfc.ReplayDetection;
 
 namespace RestartOnMiss.Utils
 {
     public partial class StuffUtils
     {
+        private static bool _shouldContinue;
         public static void BSUtilsAdd()
         {
             BSEvents.gameSceneLoaded += OnGameSceneLoaded;
@@ -24,6 +28,15 @@ namespace RestartOnMiss.Utils
         #region dumb ahh shiiii
         private static void OnGameSceneLoaded()
         {
+            _shouldContinue = UpdateShouldContinue();
+            
+            Plugin.Log.Debug($"_shouldContinue = {_shouldContinue}");
+            
+            if (!_shouldContinue)
+            {
+                return;
+            }
+            
             Plugin.Log.Debug("Game scene loaded. Attempting to find ILevelRestartController implementer...");
             RestartOnMissController.Instance.OnGameSceneLoaded();
             
@@ -47,6 +60,11 @@ namespace RestartOnMiss.Utils
 
         private static void OnNoteMissed(NoteController noteController)
         {
+            if (!_shouldContinue)
+            {
+                return; 
+            }
+            
             //Plugin.Log.Debug("note missed");
             if (RestartOnMissController.Instance != null)
             {
@@ -60,6 +78,11 @@ namespace RestartOnMiss.Utils
         
         private static void OnNoteCut(NoteController noteController, NoteCutInfo noteCutInfo)
         {
+            if (!_shouldContinue)
+            {
+                return; 
+            }
+            
             //Plugin.Log.Debug("note cut");
             if (RestartOnMissController.Instance != null)
             {
@@ -70,6 +93,35 @@ namespace RestartOnMiss.Utils
                 Plugin.Log.Warn("RestartOnMissController instance not found. Cannot restart level.");
             }
         }
+
+        /// <summary>
+        /// I realllyyyy hate doing it this way but for the life of me I could not figure out how to get it to unsubscribe and get rid of the controller so it didn't run ANYTHING
+        ///
+        /// I'll probs work on this at some point but this solution works, and it really shouldn't use a ton of resources even in fast songs or cases where this mod will even be enabled (ex not enable in vibro...)
+        /// its one if statement per note after setting this so whatever
+        /// </summary>
+        private static bool UpdateShouldContinue() 
+        {
+            if (!PluginConfig.Instance.Enabled) return false;
+            if (ReplayDetector.IsInReplay() && !PluginConfig.Instance.EnableInReplay) return false;
+            if (FPFCDetector.FPFCEnabled && !PluginConfig.Instance.EnableInFPFC) return false;
+            if (!ValidGamemode()) return false;
+            
+            return true;
+        }
+
+        public static bool ValidGamemode()
+        {
+            var levelData = BS_Utils.Plugin.LevelData;
+            if (levelData == null || !levelData.IsSet)
+            {
+                Plugin.Log.Warn("LevelData is not set. Assuming standard game mode.");
+                return false;
+            }
+            
+            return levelData.Mode != Mode.Multiplayer && levelData.Mode != Mode.Mission;
+        }
+        
         
         #endregion
     }
